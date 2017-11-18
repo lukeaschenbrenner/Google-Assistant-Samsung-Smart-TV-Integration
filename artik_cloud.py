81b234b824949dca9002898ebc038b9cbb45d9da
Florin Stancu <stancu.florin23@gmail.com>
2017-11-18 16:33:32 +0200

    first version with artik cloud from samsung

---
+++ artik_cloud.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+import configparser, json
+import samsung_smart_tv_remote as remote
+
+config = configparser.ConfigParser()
+config.read('config/artik_cloud.ini')
+
+def on_connect(client, userdata, flags, rc):
+	print "Connected with result code " + str(rc)
+	client.subscribe("/v1.1/actions/" + config['ArtikCloud']['device_id'])
+
+def on_message(client, userdata, msg):
+	print msg.topic + " " + str(msg.payload)
+	message = json.loads(msg.payload)
+	actions = message['actions'];
+	for action in actions:
+		if action['name'] == "changeChannel":
+			remote.change_channel(action['parameters']['channel'])
+		elif action['name'] == "tvOff":
+			remote.turn_off_tv()
+		else:
+			print "There is no custom command implemented for", action['name']
+
+client = mqtt.Client()
+client.username_pw_set(config['ArtikCloud']['device_id'], config['ArtikCloud']['device_token'])
+client.tls_set()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("api.artik.cloud", 8883, 60)
+
+client.loop_forever()
---
+++ samsung_smart_tv_remote.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import configparser, time, samsungctl
+
+config = configparser.ConfigParser()
+config.read('config/samsung_smart_tv_remote.ini')
+
+config_remote = {
+    "name": config['SamsungSmartTV']['name'],
+    "description": config['SamsungSmartTV']['description'],
+    "id": "",
+    "host": config['SamsungSmartTV']['host'],
+    "port": int(config['SamsungSmartTV']['port']),
+    "method": config['SamsungSmartTV']['method'],
+    "timeout": 60
+}
+
+def change_channel(channel):
+	with samsungctl.Remote(config_remote) as remote:
+		for digit in channel:
+			remote.control("KEY_" + digit)
+			time.sleep(0.5)
+		remote.control("KEY_ENTER")
+		print "The channel was changed to", channel
+
+def turn_off_tv():
+	with samsungctl.Remote(config_remote) as remote:
+		print "The TV is shutting down"
+		remote.control("KEY_POWER")
